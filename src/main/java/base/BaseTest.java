a3c58f2356544e3d3efbb13271f5fee5a5342876
Dhulipudi <2457388@cognizant.com>
2026-02-05 10:04:44 +0530

    commit in branch

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,23 +1,4 @@
-//package base;
-//
-//import utils.ConfigReader;
-//import org.openqa.selenium.WebDriver;
-//
-//public class BaseTest {
-//    public void startDriver(String browserName) 
-//    {
-//        DriverFactory.initDriver(browserName);
-//        DriverFactory.getDriver().get(ConfigReader.getString("baseURL"));
-//    }
-//
-//    public void stopDriver() 
-//    {
-//        DriverFactory.quitDriver();
-//    }
-//}
-
 package base;
-
 import org.testng.annotations.Parameters;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
@@ -26,14 +7,16 @@ import utils.ConfigReader;
 public class BaseTest {
 
     //@Parameters({"browserName"}) // This matches the parameter name in testng.xml
-    public void setUp(String browser) {
+    public void setUp(String browser)
+    {
         // Start driver with the browser passed from XML
         DriverFactory.initDriver(browser);
         DriverFactory.getDriver().get(ConfigReader.getString("baseURL"));
     }
 
     
-    public void tearDown() {
+    public void tearDown()
+    {
         DriverFactory.quitDriver();
     }
 }
